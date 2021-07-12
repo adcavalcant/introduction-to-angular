5a907875346d672c5d5856b0e1cc736183091ed7
adcavalcant <aderc19@gmail.com>
2021-07-12 10:04:18 -0400

    introduction-to-angular

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,10 @@
+//decorator que declara está classe em um componente.
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'hello',
+    template: `<h1>Hello World!</h1>`
+})
+export class HelloComponent {
+
+}
